7a540ae1621419ac454804f73d14551fa428fcd7
minha723 <minhakim0723@naver.com>
2022-07-15 15:38:56 +0900

    ProductEntity/CategoryEntity added

---
+++ src/main/java/com/example/gittest/entity/CategoryEntity.java
@@ -0,0 +1,26 @@
+package com.example.gittest.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "category_table")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(mappedBy = "categoryEntity", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ProductEntity> commentEntityList = new ArrayList<>();
+}
